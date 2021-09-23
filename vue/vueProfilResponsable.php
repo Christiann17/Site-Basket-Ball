d438d4ecb8dcf2d67a55ebffa3cfa715bf37722c
Christiann17 <91281464+Christiann17@users.noreply.github.com>
2021-09-23 19:40:35 +0200

    Delete vue directory

--- vue/vueProfilResponsable.php
+++
@@ -1,8 +0,0 @@
-<style type="text/css">
-    @import url("css/hello.css");
-</style>
-
-<fieldset name="profilresp">
-    <h5>Informations Responsable</h5>
-    <h7><B>Adresse e-mail :</B> <?= $util["mailU"] ?></h7></br>
-    <a href="./?action=pdf"><img src="images/pdf1.png" /></a>
